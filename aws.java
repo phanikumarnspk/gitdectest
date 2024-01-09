c2d0e65c026badf18c5aea36d8b348b5241c6c17
haribabu dorepalli <haribabudorepalli@gmail.com>
2024-01-09 10:28:05 +0530

    revision-Topics

---
+++ aws.java
@@ -0,0 +1,11 @@
+ in aws there are many ways to deployoing
+
+ devops
+
+ develpoment team 
+ 
+ and operational team
+
+ there are 4 ways to create instance
+
+
---
+++ devops.java
@@ -0,0 +1,10 @@
+in git there are 3 ways of storeing the data
+
+working area
+
+index area
+
+local repositary
+
+and the connection to main repositary
+
