6a047708937b7bdd678e3d56493355cfeccff8aa
ChanakyaManas <k.manasChanakya@gmail.com>
2024-01-09 10:11:20 +0530

    Chanakya files

---
+++ Scanner.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Scan{
+public statisc void main{
+Scanner in=new Scanner(System.in);
+System.out.println("Enter Your Number");
+int a=in.nextInt();
+System.out.println("Your Fav Digit:"+a);
